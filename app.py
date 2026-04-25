0a13cbde26bceb3974b352afe60fe0cb5e335e62
Jivitesh Gudekar <gudekar.jivitesh@gmail.com>
2026-04-25 14:23:20 -0300

    added code to read pdf and convert it into text

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ import streamlit as st
 # from langchain import OpenAI
 from dotenv import load_dotenv
 import os
+from PyPDF2 import PdfReader
+
+def get_pdf_text(pdf_docs):
+    text = ""
+
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+    return text
 
 def main():
     load_dotenv()  # Load environment variables from .env file
@@ -15,8 +26,13 @@ def main():
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader("Upload your documents here and click on Process", type=["pdf", "txt"], accept_multiple_files=True)
-        st.button("Process") #, on_click=process_documents)
+        if st.button("Process"):
+            with st.spinner("Processing files..."):
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+                # get text chunks
 
+                # create vector store
 
 
 
