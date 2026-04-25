1e4b084b7651a11f56cc6a6e1deba11b37b3f1e3
Jivitesh Gudekar <gudekar.jivitesh@gmail.com>
2026-04-25 14:31:06 -0300

    added telangchain_text_splitters to req.txt and code to convert text to chunks

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 from dotenv import load_dotenv
 import os
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -13,6 +15,18 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
 
     return text
+
+
+def get_text_chunks(raw_text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000, # throusand characters per chunk
+        chunk_overlap=200, # protect against cutting off important context
+        length_function=len
+    )
+    chunks = text_splitter.split_text(raw_text)
+    return chunks
+
 
 def main():
     load_dotenv()  # Load environment variables from .env file
@@ -29,9 +43,10 @@ def main():
         if st.button("Process"):
             with st.spinner("Processing files..."):
                 raw_text = get_pdf_text(pdf_docs)
-                st.write(raw_text)
+                
                 # get text chunks
-
+                text_chunks =  get_text_chunks(raw_text)
+                st.write(text_chunks)
                 # create vector store
 
 
