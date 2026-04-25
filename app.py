d96f67bbf31f65d0936dfdf1d1505d77ded76c0f
Jivitesh Gudekar <gudekar.jivitesh@gmail.com>
2026-04-25 15:56:02 -0300

    started storing the conversation in the conversation variable and stored it in session state

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
+import torch
 # from langchain import OpenAI
 from dotenv import load_dotenv
 import os
 from PyPDF2 import PdfReader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS # vectore db stores locally instead of cloud
-from langchain_openai import OpenAIEmbeddings
+# from langchain_openai import OpenAIEmbeddings # TO be removed
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_chains.memory import ConversationalBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI    
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -29,13 +35,46 @@ def get_text_chunks(raw_text):
     return chunks
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings() # create embeddings for text chunks
+    # embeddings = OpenAIEmbeddings() # create embeddings for text chunks
+    # Use Qwen3 embedding model from HuggingFaceEmbeddings instead of OpenAIEmbeddings
+    model_name = "Qwen/Qwen3-Embedding-0.6B"
+
+    model_kwargs = {
+        "device": "cuda"
+            if torch.cuda.is_available() else "cpu" # Use GPU if available
+    } 
+
+    encode_kwargs = {  
+        "normalize_embeddings": True,  # Normalize embeddings for better performance
+    }
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+    )
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings) # create vector store from text chunks and embeddings
     return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm=ChatOpenAI()  # language model to use for generating responses
+
+    memory = ConversationalBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(), # use the vector store as a retriever for relevant chunks
+        memory=memory # use conversational memory to keep track of chat history
+    )
+    return conversation_chain
+
 
 def main():
     load_dotenv()  # Load environment variables from .env file
     st.set_page_config(page_title="MultiDoc RAG Agent", page_icon=":books:")
+
+    # Initialize conversation in session state if it doesn't exist
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
 
     st.header("MultiDoc RAG Agent with LangChain and Streamlit :books:")
     st.title("MultiDoc RAG Agent")
@@ -55,7 +94,14 @@ def main():
                 # create vector store
                 vectorstore = get_vectorstore(text_chunks)
                 st.write("Files processed successfully!")
-                st.write(vectorstore)
+                
+                # Create a conversation chain with the vector store and a language model
+                # conversation is a variable that will hold the conversation chain for the user session using session state
+                # you can access the conversation chain later in the app using st.session_state.conversation outside of the sidebar
+                # session state allows you to "persist" data across user interactions in Streamlit, so the conversation chain will be available as long as the user session is active
+                st.session_state.conversation = get_conversation_chain(vectorstore) 
+
+
 
 
 if __name__ == '__main__':
